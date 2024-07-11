9bb10e8b00a5ea55d1761dac8468c675edf2c854
Fijo George <42680032+fijogeorgek@users.noreply.github.com>
2024-07-11 19:08:45 +0300

    add: shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+  </script>
+   
+  <Button>Click me</Button>
+
+<h1 class="font-black text-3xl">Hello World</h1>
+
+
+<section></section>
